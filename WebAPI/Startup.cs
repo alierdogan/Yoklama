45f6e5a6b2fe52a615e6e25d49b4d5322f5afb53
alierdogan <n.alierdogan@hotmail.com>
2023-05-15 16:38:40 +0300

    hata

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -23,7 +23,7 @@ namespace WebAPI
         }
 
         public IConfiguration Configuration { get; }
-        private  MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+        private string  MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
